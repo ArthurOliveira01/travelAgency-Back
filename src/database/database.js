255caeee3bbfc2e0381ca5556ac771b6034f79a5
Arthur Oliveira <73337580+ArthurOliveira01@users.noreply.github.com>
2023-05-29 16:42:42 -0400

    Update database.js for deploy

--- src/database/database.js
+++ src/database/database.js
@@ -1,10 +1,13 @@
-import dotenv from "dotenv";
-import pg from "pg";
+import pg from "pg"
+import dotenv from "dotenv"
+dotenv.config()
 
-dotenv.config();
+const { Pool } = pg
 
-const { Pool } = pg;
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL,
+};
 
-export const db = new Pool({
-  connectionString: process.env.DATABASE_URL
-});
+if (process.env.MODE === "prod") configDatabase.ssl = true;
+
+export const db = new Pool(configDatabase);
